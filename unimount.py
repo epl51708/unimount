2051fc06dfe8e535026ccbef9d44be8ca4da980b
Lukas E <nomail>
2016-12-08 21:32:15 +0100

    home mounting functional - security issues? + error prone

--- unimount.py
+++ unimount.py
@@ -7,8 +7,39 @@
 # lukas.epple@physik.uni-regensburg.de	#
 #---------------------------------------#
 
-import os
+import os, re, subprocess, getpass, time
+	
 
-#get username
-user = os.popen('echo $USER').read()
-print(user)
+#variables
+user	= getpass.getuser()
+mph		= "/home/"+user+"/Desktop/uni_home"		#mount point for uni /home directory
+
+#Getting NDS name
+nds = input("Please enter your NDS user name: ")
+
+#RegEx check
+nds_regex = re.compile("[a-z]{3}[0-9]{5}", 0)
+while 1:
+	if(nds_regex.match(nds)):
+		break
+	else:
+		nds = input("\nPlease enter a valid NDS user name: (e.g. vip12345)")
+		
+pw		= getpass.getpass("Please enter your NDS Password: ")
+
+#creating mointing point on local Desktop
+subprocess.call("mkdir "+mph, shell=True)
+
+#mounting uni /home in the directory mph -> mount point for home
+mnt_home  = "echo "+pw+" | "
+mnt_home += "sshfs "+nds+"@rex2.uni-regensburg.de:"
+mnt_home += "/home/"+nds+" "+mph
+mnt_home +=" -o  reconnect -o follow_symlinks -o password_stdin"
+
+subprocess.call(mnt_home, shell=True)
+
+
+#unmounting and removing locally created folder (mountpoint)
+time.sleep(10)
+subprocess.call("fusermount -u "+mph, shell=True)
+subprocess.call("rmdir "+mph, shell=True)
